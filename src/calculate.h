76d8e5ada5443d8e7306bfbec9882317526ea407
Touch_Me <109738396+Touchme245@users.noreply.github.com>
2023-09-14 20:22:47 +0300

    Update calculate.h

--- src/calculate.h
+++ src/calculate.h
@@ -4,10 +4,10 @@ int calculate(int leftborder, int rightborder){
     
     int res  {0};
     for (int i {leftborder}; i <= rightborder; ++i){
-        int num = i;
-        while (num > 0){
-            res += num % 2;
-            num = num / 2;
+        int tek_number {i};
+        while (tek_number > 0){
+            res += tek_number % 2;
+            tek_number = tek_number / 2;
 
         }
         
