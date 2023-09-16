fb34f14b6671d0fa5f364aa46c539bff77e9b4ef
Touch_Me <109738396+Touchme245@users.noreply.github.com>
2023-09-16 17:14:00 +0300

    Update calculate.h

--- src/calculate.h
+++ src/calculate.h
@@ -1,16 +1,3 @@
 #include <iostream>
 #pragma once
-int calculate(int leftborder, int rightborder){
-    
-    int res  {0};
-    for (int i {leftborder}; i <= rightborder; ++i){
-        int tek_number {i};
-        while (tek_number > 0){
-            res += tek_number % 2;
-            tek_number = tek_number / 2;
-
-        }
-        
-    }
-    return res;
-}
+int calculate(int leftborder, int rightborder);
