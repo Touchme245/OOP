9a0d91b11877945ea464bbb54475dbd3a6f95124
Touch_Me <109738396+Touchme245@users.noreply.github.com>
2023-10-04 19:54:50 +0300

    Update Six.h

--- include/Six.h
+++ include/Six.h
@@ -18,9 +18,17 @@ public:
    Six add(const Six& other);
    Six remove(const Six& other);
    bool equals(const Six& other) const;
-   bool operator>(const Six& other) const;
-   bool operator<(const Six& other) const;
+   bool greaterThan(const Six& other) const;
+   bool lessThan(const Six& other) const;
    std::ostream& print(std::ostream& os);
+
+    Six operator +(const Six& other);
+    Six operator -(const Six& other);
+    bool operator ==(const Six& other) const;
+    bool operator >(const Six& other) const;
+    bool operator <(const Six& other) const;
+    Six & operator =(const Six& other);
+    Six & operator =(Six&& other);
 
    friend class SixBuilder;
    virtual ~Six() noexcept;
@@ -28,3 +36,6 @@ private:
     long long _size;
     unsigned char *_array;
 };
+
+std::ostream &operator <<(std::ostream& os, const Six& object);
+std::istream &operator >>(std::istream& is, Six& object);
