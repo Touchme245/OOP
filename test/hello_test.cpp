e1d6482dabf800e1f54ca7752ac5397916b7f28f
Touch_Me <109738396+Touchme245@users.noreply.github.com>
2023-10-04 19:54:29 +0300

    Update hello_test.cpp

--- test/hello_test.cpp
+++ test/hello_test.cpp
@@ -1,11 +1,15 @@
 #include <gtest/gtest.h>
 #include "Six.h"
+#include "SixBuilder.h"
 
 TEST(DefaultConstructor, BasicAssertions) {
   // arrange 
-  Six obj {};
+  // через билдер не создать arr -> null лол
+ 
   int expectedSize = 0;
+  
   // act
+  Six obj {};
   int size = obj.getSize();
   unsigned char* array = obj.getArray();
 
@@ -18,19 +22,19 @@ TEST(FillConstructor, BasicAssertions) {
   // arrange 
   int n = 5;
   char val = '0';
-  Six obj(n, val);
   int expectedSize = 5;
-
   unsigned char expectedArray[] = "00000";
+  Six expectedObj = SixBuilder().size(expectedSize).data(expectedArray).build();
 
   // act
+  Six obj(n, val);
   int size = obj.getSize();
   unsigned char* array = obj.getArray();
 
   // assert
-  EXPECT_EQ(size, expectedSize);
+  EXPECT_EQ(size, expectedObj.getSize());
   for (int i =0; i < size; ++i){
-    EXPECT_EQ(array[i], expectedArray[i]);
+    EXPECT_EQ(array[i], expectedObj.getArray()[i]);
   }
   
 }
@@ -39,21 +43,25 @@ TEST(FillConstructorValueExeption, BasicAssertions) {
   // arrange 
   int n = 5;
   char val = 'k';
-
+  // assert
   EXPECT_THROW(Six obj(n, val);, std::invalid_argument);
 
 }
 
 TEST(ListConstructor, BasicAssertions) {
   // arrange 
-  Six obj {'5', '5', '5', '5'};
+  
   unsigned char expectedArray[] = "5555";
   int expectedSize = 4;
+  Six expectedObj = SixBuilder().size(expectedSize).data(expectedArray).build();
   // act
+  Six obj {'5', '5', '5', '5'};
+  
+
+  // assert
   int size = obj.getSize();
   unsigned char* array = obj.getArray();
 
-  // assert
   EXPECT_EQ(size, expectedSize);
   for (int i =0; i < size; ++i){
     EXPECT_EQ(array[i], expectedArray[i]);
@@ -64,24 +72,26 @@ TEST(ListConstructorValueExeption, BasicAssertions) {
   // arrange 
   int n = 5;
   
-
+  // assert
   EXPECT_THROW(Six obj('k','k');, std::invalid_argument);
 
 }
 
 TEST(StringConstructor, BasicAssertions) {
   // arrange 
-  Six obj {"5555"};
   unsigned char expectedArray[] = "5555";
   int expectedSize = 4;
+  Six expectedObj = SixBuilder().size(expectedSize).data(expectedArray).build();
+
   // act
-  int size = obj.getSize();
-  unsigned char* array = obj.getArray();
+  Six obj {"5555"};
 
   // assert
-  EXPECT_EQ(size, expectedSize);
+  int size = obj.getSize();
+  unsigned char* array = obj.getArray();
+  EXPECT_EQ(size, expectedObj.getSize());
   for (int i =0; i < size; ++i){
-    EXPECT_EQ(array[i], expectedArray[i]);
+    EXPECT_EQ(array[i], expectedObj.getArray()[i]);
   }
 }
 
@@ -89,35 +99,36 @@ TEST(StringConstructorValueExeption, BasicAssertions) {
   // arrange 
   int n = 5;
   
-
+  // assert
   EXPECT_THROW(Six obj{"kj"};, std::invalid_argument);
 
 }
 
 TEST(CopyConstructor, BasicAssertions) {
   // arrange 
-  Six parentObj {"5555"};
-  Six obj {parentObj};
-  unsigned char* expectedArray = parentObj.getArray();
+  unsigned char expectedArray[] = "2222";
   int expectedSize = 4;
-  // act
-  int size = obj.getSize();
-  unsigned char* array = obj.getArray();
+  Six parentObj = SixBuilder().size(expectedSize).data(expectedArray).build();
 
+  // act
+  Six obj {parentObj};
+  int size = obj.getSize();
+  
   // assert
-  EXPECT_EQ(size, expectedSize);
+  EXPECT_EQ(size, parentObj.getSize());
+  
   for (int i =0; i < size; ++i){
-    EXPECT_EQ(array[i], expectedArray[i]);
+    EXPECT_EQ(obj.getArray()[i], parentObj.getArray()[i]);
   }
 }
 
 TEST(MoveConstructor, BasicAssertions) {
   // arrange 
-  Six parentObj {'1', '1', '1', '1'};
-  Six obj(std::move(parentObj));
   unsigned char expectedArray[] = "1111";
   int expectedSize = 4;
+  Six parentObj = SixBuilder().size(expectedSize).data(expectedArray).build();
   // act
+  Six obj(std::move(parentObj));
   int size = obj.getSize();
   unsigned char* array = obj.getArray();
 
@@ -130,13 +141,16 @@ TEST(MoveConstructor, BasicAssertions) {
 
 TEST(addOperation1, BasicAssertions) {
   // arrange 
-  Six first {"1111"};
-  Six second {"1111"};
- 
+  int firstSize = 4;
+  int secondSize = 4;
+  unsigned char firstArr[] = "1111";
+  unsigned char secondArr[] = "1111";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
   unsigned char expectedArray[] = "2222";
   int expectedSize = 4;
   // act
-   Six res{first.add(second)};
+  Six res{first.add(second)};
   int size = res.getSize();
   unsigned char* array = res.getArray();
 
@@ -149,9 +163,12 @@ TEST(addOperation1, BasicAssertions) {
 
 TEST(addOperation2, BasicAssertions) {
   // arrange 
-  Six first {"12345"};
-  Six second {"12345"};
- 
+  int firstSize = 5;
+  int secondSize = 5;
+  unsigned char firstArr[] = "12345";
+  unsigned char secondArr[] = "12345";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
   unsigned char expectedArray[] = "43152";
   int expectedSize = 5;
   // act
@@ -170,8 +187,12 @@ TEST(addOperation2, BasicAssertions) {
 
 TEST(RemoveOperation1, BasicAssertions) {
   // arrange 
-  Six first {"12345"};
-  Six second {"12345"};
+  int firstSize = 5;
+  int secondSize = 5;
+  unsigned char firstArr[] = "12345";
+  unsigned char secondArr[] = "12345";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
  
   unsigned char expectedArray[] = "0";
   int expectedSize = 1;
@@ -190,9 +211,12 @@ TEST(RemoveOperation1, BasicAssertions) {
 
 TEST(RemoveOperation2, BasicAssertions) {
   // arrange 
-  Six first {"12345"};
-  Six second {"1234"};
- 
+  int firstSize = 5;
+  int secondSize = 4;
+  unsigned char firstArr[] = "12345";
+  unsigned char secondArr[] = "1234";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
   unsigned char expectedArray[] = "11111";
   int expectedSize = 5;
   // act
@@ -211,18 +235,25 @@ TEST(RemoveOperation2, BasicAssertions) {
 TEST(RemoveOperationLogicExeption, BasicAssertions) {
   // arrange 
   int n = 5;
-  Six first {"12345"};
-  Six second {"1234"};
-  
-
+  int firstSize = 5;
+  int secondSize = 4;
+  unsigned char firstArr[] = "12345";
+  unsigned char secondArr[] = "1234";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
+  // assert
   EXPECT_THROW(second.remove(first);, std::logic_error);
 
 }
 
 TEST(Equals, BasicAssertions) {
   // arrange 
-  Six first {"12345"};
-  Six second {"12345"};
+  int firstSize = 5;
+  int secondSize = 5;
+  unsigned char firstArr[] = "12345";
+  unsigned char secondArr[] = "12345";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
  
   // act
  bool res = first.equals(second);
@@ -232,27 +263,35 @@ TEST(Equals, BasicAssertions) {
   
 }
 
-// TEST(Equals2, BasicAssertions) {
-//   // arrange 
-//   Six first {"12345"};
-//   Six second {"1235"};
+TEST(Equals2, BasicAssertions) {
+  // arrange 
+  int firstSize = 5;
+  int secondSize = 4;
+  unsigned char firstArr[] = "12345";
+  unsigned char secondArr[] = "1245";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
  
-//   // act
-//  bool res = first.equals(second);
+  // act
+ bool res = first.equals(second);
   
 
-//   // assert
-//   EXPECT_EQ(res, 0);
+  // assert
+  EXPECT_EQ(res, 0);
   
-// }
+}
 
 TEST(OperatorGreter1, BasicAssertions) {
   // arrange 
-  Six first {"12345"};
-  Six second {"1235"};
+  int firstSize = 5;
+  int secondSize = 4;
+  unsigned char firstArr[] = "12345";
+  unsigned char secondArr[] = "1235";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
  
   // act
- bool res = first.operator>(second);
+ bool res = first.greaterThan(second);
   
   // assert
   EXPECT_EQ(res, 1);
@@ -261,11 +300,15 @@ TEST(OperatorGreter1, BasicAssertions) {
 
 TEST(OperatorGreter2, BasicAssertions) {
   // arrange 
-  Six first {"12345"};
-  Six second {"123555"};
+  int firstSize = 5;
+  int secondSize = 6;
+  unsigned char firstArr[] = "12345";
+  unsigned char secondArr[] = "123555";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
  
   // act
- bool res = first.operator>(second);
+ bool res = first.greaterThan(second);
   
 
   // assert
@@ -275,11 +318,15 @@ TEST(OperatorGreter2, BasicAssertions) {
 
 TEST(OperatorLower, BasicAssertions) {
   // arrange 
-  Six first {"12345"};
-  Six second {"123555"};
+  int firstSize = 5;
+  int secondSize = 6;
+  unsigned char firstArr[] = "12345";
+  unsigned char secondArr[] = "123555";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
  
   // act
- bool res = first.operator<(second);
+ bool res = first.lessThan(second);
   
 
   // assert
@@ -289,11 +336,15 @@ TEST(OperatorLower, BasicAssertions) {
 
 TEST(OperatorLower2, BasicAssertions) {
   // arrange 
-  Six first {"123434345"};
-  Six second {"123555"};
+  int firstSize = 9;
+  int secondSize = 6;
+  unsigned char firstArr[] = "123434345";
+  unsigned char secondArr[] = "123555";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
  
   // act
- bool res = first.operator<(second);
+ bool res = first.lessThan(second);
   
 
   // assert
@@ -301,3 +352,111 @@ TEST(OperatorLower2, BasicAssertions) {
   
 }
 
+TEST(Greater1, BasicAssertions) {
+  // arrange 
+  int firstSize = 5;
+  int secondSize = 6;
+  unsigned char firstArr[] = "12345";
+  unsigned char secondArr[] = "123555";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
+ 
+  // act
+ bool res = first > second;
+  
+
+  // assert
+  EXPECT_EQ(res, 0);
+  
+}
+
+TEST(Greater2, BasicAssertions) {
+  // arrange 
+  int firstSize = 5;
+  int secondSize = 6;
+  unsigned char firstArr[] = "12345";
+  unsigned char secondArr[] = "123555";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
+ 
+  // act
+ bool res = second > first;
+  
+
+  // assert
+  EXPECT_EQ(res, 1);
+  
+}
+
+TEST(lower1, BasicAssertions) {
+  // arrange 
+  int firstSize = 5;
+  int secondSize = 6;
+  unsigned char firstArr[] = "12345";
+  unsigned char secondArr[] = "123555";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
+ 
+  // act
+ bool res = first < second;
+  
+
+  // assert
+  EXPECT_EQ(res, 1);
+  
+}
+
+TEST(equality1, BasicAssertions) {
+  // arrange 
+  int firstSize = 5;
+  int secondSize = 6;
+  unsigned char firstArr[] = "12345";
+  unsigned char secondArr[] = "123555";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
+ 
+  // act
+ bool res = first == second;
+  
+
+  // assert
+  EXPECT_EQ(res, 0);
+  
+}
+
+TEST(equality2, BasicAssertions) {
+  // arrange 
+  int firstSize = 5;
+  int secondSize = 5;
+  unsigned char firstArr[] = "12345";
+  unsigned char secondArr[] = "12345";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  Six second = SixBuilder().size(secondSize).data(secondArr).build();
+ 
+  // act
+ bool res = first == second;
+  
+
+  // assert
+  EXPECT_EQ(res, 1);
+  
+}
+
+TEST(assignment, BasicAssertions) {
+  // arrange 
+  int firstSize = 5;
+  unsigned char firstArr[] = "12345";
+  Six first = SixBuilder().size(firstSize).data(firstArr).build();
+  
+ 
+  // act
+  Six res = first;
+  
+
+  // assert
+  EXPECT_EQ(res.getSize(), firstSize);
+  for (int i = 0; i < firstSize; ++i){
+     EXPECT_EQ(res.getArray()[i], first.getArray()[i]);
+  }
+  
+}
