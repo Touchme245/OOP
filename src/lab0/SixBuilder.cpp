554b6b03158860be5cf6fd6d2b3098ce56f53772
Touch_Me <109738396+Touchme245@users.noreply.github.com>
2023-10-04 19:53:50 +0300

    Update SixBuilder.cpp

--- src/lab0/SixBuilder.cpp
+++ src/lab0/SixBuilder.cpp
@@ -10,11 +10,21 @@ SixBuilder& SixBuilder::size(long long value){
 }
 
 SixBuilder& SixBuilder::data(unsigned char * value){
-    object._array = value;
+    if (value == nullptr) {
+        throw std::invalid_argument("Null is not permitted value");
+    }
+    object._array = new unsigned char[object._size]; 
+
+    for (long long i = 0; i < object._size; i++) {
+        object._array[object._size-1-i] = value[i];
+    }
     return *this;
 }
+
 
 Six SixBuilder::build(){
     return object;
 }
+
+
 
